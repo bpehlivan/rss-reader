4db12a162a01d0920dd680244edf07c8c6f2be3c
burkay <bpehlivan235@gmail.com>
2024-02-24 22:12:40 +0300

    refactor pydantic schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,13 +1,14 @@
-from pydantic import BaseModel, ConfigDict, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
 
 class UserIn(BaseModel):
     email: EmailStr
-    password: str
-    full_name: str
+    password: str = Field(min_length=8, max_length=64)
+    full_name: str = Field(min_length=5, max_length=64)
 
 
 class UserOut(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     email: str
     full_name: str
+    id: int
